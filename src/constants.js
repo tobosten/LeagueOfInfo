ea330abbf93fcd61b23a49cfa3f53b3a25e4fffe
Erizium <tobias.osterlin@gmail.com>
2022-04-25 17:04:13 +0200

    Created new array in constants.js which contain all mastery titles, which will be displayed over or under your mastery icon.

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,69 @@
 
 
 export const constants = {
-    api_key: `?api_key=RGAPI-f81d7275-5b69-4c80-97dc-ae0220ac6bcd`
+    api_key: `?api_key=RGAPI-f81d7275-5b69-4c80-97dc-ae0220ac6bcd`,
+
+    masteryTitles: {
+        mastery1: {
+            Assassin: "Rogue",
+            Fighter: "Scrapper",
+            Mage: "Initiate",
+            Marksman: "Tracker",
+            Support: "Aide",
+            Tank: "Grunt",
+        }, 
+        mastery2: {
+            Assassin: "Prowler",
+            Fighter: "Brawler",
+            Mage: "Conjurer",
+            Marksman: "Strider",
+            Support: "Protector",
+            Tank: "Bruiser",
+        },
+        mastery3: {
+            Assassin: "Cutthroat",
+            Fighter: "Warrior",
+            Mage: "Invoker",
+            Marksman: "Scout",
+            Support: "Keeper",
+            Tank: "Bulwark",
+        },
+        mastery4: {
+            Assassin: "Reaper",
+            Fighter: "Veteran",
+            Mage: "Magus",
+            Marksman: "Ranger",
+            Support: "Defender",
+            Tank: "Enforcer",
+        },
+
+        mastery5: {
+            Assassin: "Slayer",
+            Fighter: "Destroyer",
+            Mage: "Warlock",
+            Marksman: "Pathfinder",
+            Support: "Guardian",
+            Tank: "Brute",
+        },
+
+        mastery6: {
+            Assassin: "Executioner",
+            Fighter: "Warmonger",
+            Mage: "Sorcerer",
+            Marksman: "Sharpshooter",
+            Support: "Sentinel",
+            Tank: "Colossus",
+        },
+
+        mastery7: {
+            Assassin: "Deathmaster",
+            Fighter: "Warlord",
+            Mage: "Archmage",
+            Marksman: "Sniper",
+            Support: "Warden",
+            Tank: "Juggernaut",
+        },
+    }
+
+    
 }
